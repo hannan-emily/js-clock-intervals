113d689517ea8a39a642cb7bb9a126e7b1c03a41
emily <emilyhannan@D.local>
2018-01-25 18:10:26 -0800

    finished files to make clock work

--- README.md
+++ README.md
@@ -62,4 +62,3 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 ## Licensing
 All content is licensed under a CC­BY­NC­SA 4.0 license.
 All software code is licensed under GNU GPLv3. For commercial use or alternative licensing, please contact legal@ga.co.
-
---
+++ clock.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+  //code and events (DOM) go here - this is an event listener for the entire document
+
+  var degrees = 0;
+
+  //rotate second hand
+  var second = document.getElementById("second");
+
+  setInterval(function(){
+    second.style.transform = "rotate(" + degrees + "deg)";
+    degrees = degrees + 6;
+  }, 1000);
+
+  //rotate minute hand
+
+  var degrees2 = 0;
+
+  var minute = document.getElementById("minute");
+
+  setInterval(function(){
+    minute.style.transform = "rotate(" + degrees2 + "deg)";
+    degrees2 = degrees2 + 6;
+  }, 6000);
+
+  //rotate hour hand
+  var degrees3 = 0;
+
+  var hour = document.getElementById("hour");
+
+  setInterval(function(){
+    hour.style.transform = "rotate(" + degrees3 + "deg)";
+    degrees3 = degrees3 + 5;
+  }, 3.6e+6);
+
+
+});
